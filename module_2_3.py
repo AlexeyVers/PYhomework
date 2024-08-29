8fb5bdddb4b6e10d3e7035282f299fa412dd1bf3
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-08-30 00:16:04 +0300

    Create module_2_3.py

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = len(my_list)
+static_count = 0
+while static_count < count:
+    if my_list[static_count] >= 0:
+        print(my_list[static_count])
+        static_count = static_count + 1
+    else:
+        break
