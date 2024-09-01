50bf1d1f3490774bc5a63fad193f1ab249071b0c
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-09-01 15:26:04 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -3,17 +3,20 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 is_prime = True
+includes = []
 for i in numbers:
-    for j in range(2, i-1):
+    for j in range(2, i):
         if i % j == 0:
             is_prime = False
             break
         else:
             is_prime = True
-    if is_prime == True:
-        primes.append(i)
+    if is_prime:
+        if i == 1:
+            includes.append(i)
+        else:
+            primes.append(i)
     else:
         not_primes.append(i)
-
 
 print('Простые: ', primes, 'Не простые: ', not_primes)
