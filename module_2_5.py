4f72f51194797a9dd8fb6afb9f14d5f20dfe6283
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-09-01 16:14:19 +0300

    Create module_2_5.py

---
+++ module_2_5.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+result1 = get_matrix(2, 6, 12)
+print(result1)
