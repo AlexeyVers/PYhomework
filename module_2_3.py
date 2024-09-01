3f93ecbed5e5c1952a755edf35fda86805763976
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-09-01 12:21:14 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,8 +2,10 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 count = len(my_list)
 static_count = 0
 while static_count < count:
-    if my_list[static_count] > 0:
+    if my_list[static_count] == 0:
+        static_count = static_count + 1
+    elif my_list[static_count] > 0:
         print(my_list[static_count])
         static_count = static_count + 1
     else:
-        static_count = static_count + 1
+        break
