446460269d526f3c2afef50d8a960afb1ff98798
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-09-02 21:41:16 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,13 +1,7 @@
-main_list = []
-decision_list = {}
 for i in range(3, 21):
-    main_list.append(i)
-
-for i in main_list:
-    list_ = []
-    for j in range(1, i+1):
-        for u in range(1, i+1):
-            if i >= j + u and (j + u) % i == 0:
-                list_.append([j, u])
-    decision_list[i] = list_
-print(decision_list)
+    key_ = str()
+    for k in range(1, i + 1):
+        for u in range(1, i + 1):
+            if i >= k + u and (k + u) % i == 0:
+                key_ = str(key_) + str(k) + str(u)
+    print(i, '-', key_)
