22862ab683180977e196fe0be3ff6d331ac2252e
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2024-09-19 12:25:44 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -4,8 +4,9 @@ def custom_write(file_name, strings):
     strings_positions = {}
     write_ = open(file_name, 'a', encoding='utf-8')
     for i in range(len(strings)):
+        num_strok = write_.tell()
         write_.write(f'{strings[i]}\n')
-        strings_positions[(i + 1, write_.tell())] = strings[i]
+        strings_positions[(i + 1, num_strok)] = strings[i]
     write_.close()
     return strings_positions
 
